2199c6d65acb247b5e2f28ce80334a2ae2baf1f0
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-08-22 08:36:22 -0300

    Atributo - typo - escritura - dif entre no derivado y derivado

--- src/compilerphp/actions/ReadModel.java
+++ src/compilerphp/actions/ReadModel.java
@@ -10,34 +10,30 @@ import java.util.List;
 
 public class ReadModel{
 	
-
-	
-	public static void test(){
-		//WriteSQL.put("SQL");
-		//WriteSQL.escribir();
-	}
-	
-
+	private static String path="/home/leo/elearning.metawebdesign";
 	
 	public static void loadXML() throws IOException
 	{
 			SQL sql= new SQL();
 			Tabla t=  new Tabla();
 			
-			String substr;
-			FileReader fr = new FileReader("/home/leo/default.metawebdesign");
+			FileReader fr = new FileReader(path);//LECTURA DEL ARCHIVO DEL MODELO
 			BufferedReader br = new BufferedReader(fr);
-			String line;
-			int cont_tabla=0;
+			String line;//LINEA DE LECTURA DEL ARCHIVO
+			int cont_tabla=0;//CONTADOR DE TABLAS
 			
 			//LECTURA
-			while((line = br.readLine()) != null) {	
+			while((line = br.readLine()) != null) {
+				
+				//System.out.println(line);
 				int x_class=line.indexOf("class name="); //IDENTIFICA UNA CLASE
 				int x_attribute=line.indexOf("<hasAttributes");//IDENTIFICA UN ATRIBUTO
+				int x_attribute_not_derived=line.indexOf("metawebdesign:NotDerived");
+				int x_attribute_derived=line.indexOf("metawebdesign:Derived");
 				int x_attribute_name=line.indexOf("name=");//POSICION DEL NOMBRE DEL ATRIBUTO
+				int x_attribute_type=line.indexOf("dataType=");
 				int x_relation=line.indexOf("hasRelationClass name=");//IDENTIFICA UNA RELACION
 				int x_relation_to_class=line.indexOf("fromClass=");//IDENTIFICA LA OTRA CLASE EN LA RELACION
-				
 				//BUSQUEDA TABLA
 				if(x_class != -1){
 					//AGREGO LA TABLA IDENTIFICADA ANTERIORMENTE
@@ -47,21 +43,36 @@ public class ReadModel{
 					
 					//NUEVA TABLA
 					t = new Tabla();
-			        substr = line.substring(x_class+12, line.length()-2);
-			        t.setNombre(substr);//AGREGO EL NOMBRE A LA TABLA
+			        String substr = line.substring(x_class+12, line.length());
+			        int stop=substr.indexOf("\"");//CRITERIO DE PARADA PARA EXTRACCION DEL DATO
+			        String tabla=substr.substring(0, stop);
+			        t.setNombre(tabla);//AGREGO EL NOMBRE A LA TABLA
+			        System.out.println("Tabla nombre: "+tabla);
 			        cont_tabla++;
 			    }
 				
-				//BUSQUEDA ATRIBUTO
-				if(x_attribute != -1){
-			        substr = line.substring(x_attribute_name+6, line.length()-3);
-			        Atributo a = new Atributo(substr, false, false, "text");
+				//BUSQUEDA ATRIBUTO NO DERIVADO
+				if(x_attribute != -1 && x_attribute_not_derived != -1){
+					//DATOS A EXTRAER DE LOS ATRIBUTOS
+			        String substr_nombre = line.substring(x_attribute_name+6, line.length());
+			        String substr_type=line.substring(x_attribute_type+10, line.length());
+			        
+			        //CRITERIO DE PARADA PARA EXTRACCION DEL DATO
+			        int stop_nombre=substr_nombre.indexOf("\"");
+			        int stop_type=substr_type.indexOf("\"");
+			        
+			        String atributo_nombre=substr_nombre.substring(0, stop_nombre);
+			        String atributo_type=substr_type.substring(0, stop_type);
+			        
+			        System.out.println("Atributo nombre: "+atributo_nombre+" tipo: "+atributo_type);
+			        Atributo a = new Atributo(atributo_nombre, false, false, atributo_type);
 			        t.addAtributo(a);//AGREGO ATRIBUTOS A LA TABLA 
 				}
+				
 				//BUSQUEDA RELACION
 				if(x_relation != -1){
 					//NOMBRE EN LA RELACION
-			        substr = line.substring(x_relation+23, x_relation_to_class-2);
+			        String substr = line.substring(x_relation+23, x_relation_to_class-2);
 			        
 			        //OTRA TABLA INVOLUCRADA
 			        substr = line.substring(x_relation_to_class+14, line.length()-3);
@@ -70,7 +81,7 @@ public class ReadModel{
 				
 			}
 			fr.close();
-			
+			sql.addTabla(t);//AGREGO LA ULTIMA TABLA
 			WriteSQL.write(sql);
 			
 	}
