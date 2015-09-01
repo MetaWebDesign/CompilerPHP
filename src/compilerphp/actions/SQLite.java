61ebf29f75e367a7c36b00eae2ff7c37e605f8d8
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-08-31 21:59:22 -0300

    Mode genSQL - mod de forma de generar el SQL

--- src/compilerphp/actions/SQLite.java
+++ src/compilerphp/actions/SQLite.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLite{
@@ -15,22 +16,23 @@ public class SQLite{
 	private static String fileSQL="sql.dat";
 	
 	//ESCRIBRE EN TEXTO PLANO LAS SENTENCIAS SQL
-	public static void write(SQL sql, String path){
-		System.out.println("Write XML, path: "+path);
+	public static List<String> genSQL(SQL sql, String path){
 		List <View> views = sql.getViews();
 		List <Tabla> tablas = sql.getTablas();
 		List <Atributo> atributos;
 		List <ForeignKey> foreignKeys;
+		List <String> dataBase = new ArrayList<String>();
 		//ESCRITURA DE LA NUEVA SENTENCIA
 		FileWriter fichero = null;
-		try {
-			fichero = new FileWriter(path+"/"+fileSQL);
+		//try {
+		//	fichero = new FileWriter(path+"/"+fileSQL);
 			
 			//ESCRIVO LAS TABLAS
 			for(Tabla tabla : tablas) {
+				String linea_sql="";
 				// ESCRIVO EL NOMBRE DE LA TABLA
-			    fichero.write("\n CREATE TABLE "+tabla.getNombre()+" (");
-			    
+		//	    fichero.write("\n CREATE TABLE "+tabla.getNombre()+" (");
+			    linea_sql=linea_sql+"CREATE TABLE "+tabla.getNombre()+" (";
 			    //CAPTURO LOS ATRIBUTOS DE LA TABLA
 			    atributos=tabla.getAtributos();
 			    foreignKeys=tabla.getForeignKeys();
@@ -40,18 +42,23 @@ public class SQLite{
 			    for(Atributo atributo : atributos){
 			    	//ESCRIVO LOS ATRIBUTOS
 			    	if(coma !=0){
-			    		fichero.write(", ");
+			    		//fichero.write(", ");
+			    		linea_sql=linea_sql+", ";
 			    	}
 			    	
-			    	fichero.write(atributo.getNombre());
+			    	//fichero.write(atributo.getNombre());
+			    	linea_sql=linea_sql+atributo.getNombre();
 			    	
 			    	if (atributo.getType().equals("autoincremental")){
-			    		fichero.write(" integer primary key not null");
+			    		//fichero.write(" integer primary key not null");
+			    		linea_sql=linea_sql+" integer primary key not null";
 			    	}
 			    	if (!atributo.getType().equals("autoincremental")){
-			    		fichero.write(" "+atributo.getType());
+			    		//fichero.write(" "+atributo.getType());
+			    		linea_sql=linea_sql+" "+atributo.getType();
 			    		if(atributo.getPrimaryKey() == true){
-			    			fichero.write(" PRIMARY KEY");
+			    			//fichero.write(" PRIMARY KEY");
+			    			linea_sql=linea_sql+" PRIMARY KEY";
 			    		}
 			    	}
 			    	
@@ -64,56 +71,66 @@ public class SQLite{
 		    		String atributoDestino=tablas.get(fk.getDestination()).getAtributos().get(fk.getAtributoDestination()).getNombre();
 		    		String typeAtributo=tablas.get(fk.getDestination()).getAtributos().get(fk.getAtributoDestination()).getType();
 		    		
-		    		fichero.write(", "+fk.getNombre()+" ");
+		    		//fichero.write(", "+fk.getNombre()+" ");
+		    		linea_sql=linea_sql+", "+fk.getNombre()+" ";
 		    		if(typeAtributo.equals("autoincremental")){
-		    			fichero.write("integer ,");
+		    			//fichero.write("integer ,");
+		    			linea_sql=linea_sql+"integer ,";
 		    		}
-		    		fichero.write(" FOREIGN KEY("+fk.getNombre()+") REFERENCES "+tablaDestino+"("+atributoDestino+")");
+		    		//fichero.write(" FOREIGN KEY("+fk.getNombre()+") REFERENCES "+tablaDestino+"("+atributoDestino+")");
+		    		linea_sql=linea_sql+" FOREIGN KEY("+fk.getNombre()+") REFERENCES "+tablaDestino+"("+atributoDestino+")";
 		    		/*
 		    		System.out.println("Tabla :"+tablas.get(fk.getDestination()).getNombre());
 		    		System.out.println("Atributo :"+tablas.get(fk.getDestination()).getAtributos().get(fk.getAtributoDestination()).getNombre());
 		    		System.out.println("Typo :"+tablas.get(fk.getDestination()).getAtributos().get(fk.getAtributoDestination()).getType());
 		    		*/
 		    	}
-			    
-			    fichero.write(");");
+		    	System.out.println(linea_sql);
+		    	
+			    //fichero.write(");");
+		    	linea_sql=linea_sql+");";
+		    	dataBase.add(linea_sql);
 	        }
 			
 			//ESCRIVO LAS VISTAS
 			for(View view : views){
-				fichero.write("\n CREATE VIEW "+view.getTabla()+view.getNombre()+"view as ");
-				fichero.write("select * from "+view.getTabla());
-				fichero.write(", ("+view.getFormula()+") as derived");
+				String vista="CREATE VIEW "+view.getTabla()+view.getNombre()+"view as ";
+				vista=vista+"select * from "+view.getTabla();
+				vista=vista+", ("+view.getFormula()+") as derived";
+				dataBase.add(vista);
 			}
 			
-			fichero.close();
-		}catch (Exception ex){
-			System.out.println("Mensaje de la excepción: " + ex.getMessage());
-		}
+		//	fichero.close();
+		//}catch (Exception ex){
+		//	System.out.println("Mensaje de la excepción: " + ex.getMessage());
+		//}
+			
+		return dataBase;
 	}
 	
 	//GENERO LA BASE DE DATOS EN BASE AL ARCHIVO SQL.DAT ESCRITO ANTEIORMENTE
-	public static void createDB(SQL sql, String path, String file) throws IOException{
+	public static void createDB(SQL model, String path, String file) throws IOException{
 		//GENERO EL CODIGO SQL
-		SQLite.write(sql, path);
-		ExecuteShellComand obj = new ExecuteShellComand();
+		List <String> dataBase=SQLite.genSQL(model, path);
+		//ExecuteShellComand obj = new ExecuteShellComand();
 		//GENERO EN BASE AL COD SQL LA BASE DE DATOS SQLITE
-		try {
-			FileReader fr = new FileReader(path+"/"+fileSQL);
-			BufferedReader br = new BufferedReader(fr);
-			String sql_line;//LINEA DE LECTURA DEL ARCHIVO
-			System.out.println("Generando la base de datos");
+		//try {
+		//	FileReader fr = new FileReader(path+"/"+fileSQL);
+		//	BufferedReader br = new BufferedReader(fr);
+		//	String sql_line;//LINEA DE LECTURA DEL ARCHIVO
+		//	System.out.println("Generando la base de datos");
 			//LECTURA
-			while((sql_line = br.readLine()) != null) {
-				//obj.executeCommand("sqlite3 "+path+"/"+file+".db "+"'"+line+"';");//UBICA EL ARCHIVO XML
+		//	while((sql_line = br.readLine()) != null) {
+		//		//obj.executeCommand("sqlite3 "+path+"/"+file+".db "+"'"+line+"';");//UBICA EL ARCHIVO XML
+		for(String sql_line : dataBase){
 				sqlite_execute(sql_line, path);
-				System.out.println(sql_line);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Error al leer el archivo sql.dat");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				System.out.println("sqlite3-> "+sql_line);
 		}
+		//} catch (FileNotFoundException e) {
+		//	System.out.println("Error al leer el archivo sql.dat");
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
 		
 	}
 
