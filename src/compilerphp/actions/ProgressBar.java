0ea6ee8f4a1a01aa465d374f02c1f26761bbb10d
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-09-11 19:15:43 -0300

    Implementación de Barra de progreso de las actividades del compilador

---
+++ src/compilerphp/actions/ProgressBar.java
@@ -0,0 +1,42 @@
+package compilerphp.actions;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.border.Border;
+
+public class ProgressBar{
+	
+	static JFrame f ;
+	 Border border;
+	static Container content;
+	static JProgressBar progressBar;
+	
+	public ProgressBar(String title){
+		  
+		  f= new JFrame(title);
+		  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  content = f.getContentPane();
+		  progressBar = new JProgressBar();
+		    
+		    progressBar.setValue(0);
+		    progressBar.setStringPainted(true);
+		    border = BorderFactory.createTitledBorder("Iniciando...");
+		    progressBar.setBorder(border);
+		    content.add(progressBar, BorderLayout.NORTH);
+		    f.setSize(300, 100);
+		    f.setVisible(true);
+	}
+	
+	public void updateProgress(int porcentage, String msn){
+	    progressBar.setValue(porcentage);
+	    progressBar.setStringPainted(true);
+	    border = BorderFactory.createTitledBorder(msn);
+	    progressBar.setBorder(border);
+	    content.add(progressBar, BorderLayout.NORTH);
+	}
+	
+}
--- src/compilerphp/actions/Test.java
+++ src/compilerphp/actions/Test.java
@@ -1,55 +1,12 @@
 package compilerphp.actions;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.SwingUtilities;
 
 public class Test extends JPanel {
 
-  JProgressBar pbar;
-
-  static final int MY_MINIMUM = 0;
-
-  static final int MY_MAXIMUM = 100;
-
-  public Test() {
-    // initialize Progress Bar
-    pbar = new JProgressBar();
-    pbar.setMinimum(MY_MINIMUM);
-    pbar.setMaximum(MY_MAXIMUM);
-    // add to JPanel
-    add(pbar);
-  }
-
-  public void updateBar(int newValue, String msj) {
-    pbar.setValue(newValue);
-    //pbar.setNote(msj);
-  }
-
   public static void main(String args[]) {
-
-    final Test it = new Test();
-
-    JFrame frame = new JFrame("Progress Bar Example");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setContentPane(it);
-    frame.pack();
-    frame.setVisible(true);
-
-    // run a loop to demonstrate raising
-    for (int i = MY_MINIMUM; i <= MY_MAXIMUM; i++) {
-      final int percent = i;
-      try {
-        SwingUtilities.invokeLater(new Runnable() {
-          public void run() {
-            it.updateBar(percent, "Hola");
-          }
-        });
-        java.lang.Thread.sleep(100);
-      } catch (InterruptedException e) {
-        ;
-      }
-    }
+	  ProgressBar bar=new ProgressBar("Prueba de barra"); 
+	  bar.updateProgress(10, "10% de wea");
+	  
   }
 }
