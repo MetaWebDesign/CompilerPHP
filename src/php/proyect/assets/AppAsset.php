af64552852c44a4bbf77220a4b456d233e11caa9
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-09-16 02:05:25 -0300

    Assets update - Solución conflicto Composer DateTimePicker

--- src/php/proyect/assets/AppAsset.php
+++ src/php/proyect/assets/AppAsset.php
@@ -21,17 +21,18 @@ class AppAsset extends AssetBundle
         'css/site.css',
 		//'css/bootstrap.min.css',
 		//'css/font-awesome.min.css',
-		//'css/font-awesome.css',
+		'css/font-awesome.css',
 		'css/metisMenu.min.css',
 		'css/sb-admin-2.css',
 		'css/timeline.css',
     ];
     public $js = [
-		'js/bootstrap.min.js',
-		'js/jquery.min.js',
+
+		//'js/bootstrap.min.js',
+		//'js/jquery.min.js',
 		'js/metisMenu.min.js',
 		'js/morris.min.js',
-		'js/morris-data.js',
+		//'js/morris-data.js',
 		'js/raphael-min.js',
 		'js/sb-admin-2.js',	
     ];
