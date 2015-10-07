2d3841c74de8da7d394ed029ad72688d6b12493f
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-10-07 14:54:55 -0300

    Fix Acceso a premisos - un servicio de una clase puede tener más de un rol para acceder

--- src/php/proyect/components/Access.php
+++ src/php/proyect/components/Access.php
@@ -12,16 +12,18 @@ class Access{
 
   public function validate($id_user_rol, $clase_name, $service){
     $vista = Dashboard::find()->where(['nombre'=>$clase_name])->one();
-    $dash_crud = DashboardPermisoscrud::find()->where(['id_dash'=>$vista->id, 'service'=>$service])->one();
+    $dash_crud = DashboardPermisoscrud::find()->where(['id_dash'=>$vista->id, 'service'=>$service])->all();
     if($id_user_rol == 1){
-      return true;
-    }
-    if($id_user_rol == $dash_crud->id_rol){
-      return true;
+    return true;
     }
     else{
-      return false;
+      foreach($dash_crud AS $permiso){
+        if($id_user_rol == $permiso->id_rol){
+          return true;
+        }
+      }
     }
+      return false;
   }
 
 }
